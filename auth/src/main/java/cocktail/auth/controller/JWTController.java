41f7398085356d775df60213b7ea93ba2dd6b95d
hyunkyungju <wngusrudrid@naver.com>
2022-12-21 16:29:22 +0900

    [Feat] gateway에서 필터 거치는 코드 추가

--- auth/src/main/java/cocktail/auth/controller/JWTController.java
+++ auth/src/main/java/cocktail/auth/controller/JWTController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
 
-@Controller
+
 @RestController
 @RequestMapping("/auth")
 public class JWTController {
--- gateway/src/main/java/cocktail/gateway/Config/AuthorizationHeaderFilter.java
+++ gateway/src/main/java/cocktail/gateway/Config/AuthorizationHeaderFilter.java
@@ -1,9 +1,21 @@
 package cocktail.gateway.Config;
 
+import io.jsonwebtoken.ExpiredJwtException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.reactive.error.ErrorWebExceptionHandler;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<AuthorizationHeaderFilter.Config> {
@@ -22,7 +34,45 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
         return (exchange, chain) -> {
             String token = exchange.getRequest().getHeaders().get("Authorization").get(0);//.substring(7);
             System.out.println("token = " + token);
+            Map<String, Object> userInfo = new HashMap<>();
+            userInfo.put("memberId", "1");
+            addAuthorizationHeaders(exchange.getRequest(), userInfo);
+
             return chain.filter(exchange);
         };
     }
+
+    private void addAuthorizationHeaders(ServerHttpRequest request, Map<String, Object> userInfo) {
+        request.mutate()
+                .header("User-Id", userInfo.get("memberId").toString())
+                .build();
+    }
+
+
+    @Bean
+    // 예외 처리 핸들러
+    public ErrorWebExceptionHandler tokenValidation(){
+        return new JwtTokenExceptionHandler();
+    }
+
+    public class JwtTokenExceptionHandler implements ErrorWebExceptionHandler{
+        private String getErrorCode(int errorCode){
+            return "{\"errorCode\":" + errorCode + "}";
+        }
+
+        @Override
+        public Mono<Void> handle(ServerWebExchange exchange, Throwable ex) {
+            int errorCode = 500;
+            if (ex.getClass() == NullPointerException.class){
+                errorCode = 100;
+            } else if (ex.getClass() == ExpiredJwtException.class){
+                errorCode = 200;
+            }
+
+            byte[] bytes = getErrorCode(errorCode).getBytes(StandardCharsets.UTF_8);
+            DataBuffer buffer = exchange.getResponse().bufferFactory().wrap(bytes);
+            return exchange.getResponse().writeWith(Flux.just(buffer));
+        }
+    }
+
 }
