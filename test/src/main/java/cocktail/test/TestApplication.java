a5fc356e721092145b7fdd9192b1bc559324549c
hyunkyungju <wngusrudrid@naver.com>
2022-12-21 17:42:36 +0900

    [Refactor] test 서버에서 eureka 사용하지 않음

--- test/src/main/java/cocktail/test/TestApplication.java
+++ test/src/main/java/cocktail/test/TestApplication.java
@@ -2,10 +2,8 @@ package cocktail.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class TestApplication {
 
 	public static void main(String[] args) {
