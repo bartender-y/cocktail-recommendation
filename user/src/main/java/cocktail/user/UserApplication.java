148179246827eb071fee677b3c3d2558f8f26855
hyunkyungju <wngusrudrid@naver.com>
2022-12-21 13:26:57 +0900

    [Feat] user 서비스 유레카 설정 추가

--- user/src/main/java/cocktail/user/UserApplication.java
+++ user/src/main/java/cocktail/user/UserApplication.java
@@ -2,13 +2,13 @@ package cocktail.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-// @EnableEurekaClient
+@EnableEurekaClient
 public class UserApplication {
 
 	@Bean
