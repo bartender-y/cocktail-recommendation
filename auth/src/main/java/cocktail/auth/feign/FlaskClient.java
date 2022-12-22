3b20bb12f19546c6d533d0f9df9dd8213d867750
hyunkyungju <wngusrudrid@naver.com>
2022-12-22 14:31:15 +0900

    [Feat] 플라스크 url 환경변수로 받음

--- auth/src/main/java/cocktail/auth/feign/FlaskClient.java
+++ auth/src/main/java/cocktail/auth/feign/FlaskClient.java
@@ -1,11 +1,12 @@
 package cocktail.auth.feign;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "flask-client", url = "http://localhost:5000")
+@FeignClient(name = "flask-client", url = "${flask.url}")
 public interface FlaskClient {
 
     @PostMapping(value = "/flask/drink-bti")
