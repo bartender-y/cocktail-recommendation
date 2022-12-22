b7d1850a1eca598c73bc09a7bb1e62abb0d09379
jk121925 <52795602+jk121925@users.noreply.github.com>
2022-12-22 14:40:32 +0900

    [Fix] front 폴더 lock제거

--- build.sh
+++ build.sh
@@ -4,14 +4,16 @@ set +x
 set +e
 
 cd discovery
-
+chmod +x gradlew
 ./gradlew clean build -x test
 cd ../
 
 cd gateway
+chmod +x gradlew
 ./gradlew clean build -x test
 cd ../
 
 cd auth
+chmod +x gradlew
 ./gradlew clean build -x test
 cd ../
